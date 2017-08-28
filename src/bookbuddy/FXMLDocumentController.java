1a906f832a4ad73facf7cb20baeabd33e06bb9cd
Tobi <Tobi@192.168.1.23>
2017-08-28 20:28:21 +0200

    book buddy base files

---
+++ src/bookbuddy/FXMLDocumentController.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bookbuddy;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+/**
+ *
+ * @author Tobi
+ */
+public class FXMLDocumentController implements Initializable {
+    
+    @FXML
+    private TextField searchTextField;
+    @FXML
+    private Button startSearchBtn;
+    
+    @FXML
+    private void handleButtonAction(ActionEvent event) {
+        System.out.println("You clicked me!");
+        searchTextField.setText("Hello World!");
+        startSearchBtn.setText("Clicked!");
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+        
+    }    
+    
+}
